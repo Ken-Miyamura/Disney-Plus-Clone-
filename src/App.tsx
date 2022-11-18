5355dad630efed9c8f0b032a7b727b2e812e098c
“Your <mykmiyamu@gmail.com>
2022-11-18 20:03:17 +0900

    create movies & details route-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Detail from './components/Detail';
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
     </div>
   )
 }
